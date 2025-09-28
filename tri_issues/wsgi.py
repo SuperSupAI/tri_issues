4befe38039cdf6888d98268e0583c9efba127207
June <peedapus@gmail.com>
2025-09-28 21:48:50 +0700

    wsgi.py

--- tri_issues/wsgi.py
+++ tri_issues/wsgi.py
@@ -7,12 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os,sys,site
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('D:/Python/projects/tri_issues')
-sys.path.append('D:/Python/projects/tri_issues/tri_issues')
+#sys.path.insert(0, 'C:/Users/Administrator/AppData/Local/Programs/Python/Python312/Lib/site-packages')
+sys.path.append('D:/FTP/TRI_ISSUES')
+sys.path.append('D:/FTP/TRI_ISSUES/tri_issues')
+#sys.path.insert(0, 'D:/Python/projects/tri_issues')
+#sys.path.insert(0, 'D:/Python/projects/tri_issues/tri_issues')
 
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tri_issues.settings')
 os.environ["DJANGO_SETTINGS_MODULE"] = "tri_issues.settings"
